10b8f6110edd467f792a4105ceea6508256239be
devmuzammil <muzammilhussainabro@gmail.com>
2026-09-24 15:27:50 +0500

    FizzBuzz Exercise Done

--- week1/basics.py
+++ week1/basics.py
@@ -1,3 +1,4 @@
+# EX Letter Grade
 def grade(score):
     if(score >= 90):
         return 'A'
@@ -13,3 +14,19 @@ def grade(score):
 result=grade(79)
 
 print(result)
+
+# Ex FizzBuzz 1-100
+
+def fizz_buzz():
+        if number % 3 == 0 and number % 5 == 0:
+            return 'FizzBuzz'
+        elif number % 3 == 0:
+            return 'Fizz'
+        elif number % 5 == 0:
+            return 'Buzz'
+        else:
+            return number
+
+for number in range(1,101):
+     play_fizz_buzz=fizz_buzz()
+     print(play_fizz_buzz)
