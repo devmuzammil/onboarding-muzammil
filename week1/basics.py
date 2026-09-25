a854b27c00c943b6204cdc8d37f9804311683677
devmuzammil <muzammilhussainabro@gmail.com>
2026-09-25 11:17:06 +0500

    Uncommented the code

--- week1/basics.py
+++ week1/basics.py
@@ -1,77 +1,77 @@
-# import random
-# # EX Letter Grade
-# def grade(score):
-#     if(score >= 90):
-#         return 'A'
-#     elif(score >=80):
-#         return 'B'
-#     elif(score>=70):
-#         return 'C'
-#     elif(score >=60):
-#         return 'D'
-#     else:
-#         return 'F'
+import random
+# EX Letter Grade
+def grade(score):
+    if(score >= 90):
+        return 'A'
+    elif(score >=80):
+        return 'B'
+    elif(score>=70):
+        return 'C'
+    elif(score >=60):
+        return 'D'
+    else:
+        return 'F'
 
-# result=grade(79)
+result=grade(79)
 
-# print(result)
+print(result)
 
-# # Ex FizzBuzz 1-100
+# Ex FizzBuzz 1-100
 
-# def fizz_buzz():
-#         if number % 3 == 0 and number % 5 == 0:
-#             return 'FizzBuzz'
-#         elif number % 3 == 0:
-#             return 'Fizz'
-#         elif number % 5 == 0:
-#             return 'Buzz'
-#         else:
-#             return number
+def fizz_buzz():
+        if number % 3 == 0 and number % 5 == 0:
+            return 'FizzBuzz'
+        elif number % 3 == 0:
+            return 'Fizz'
+        elif number % 5 == 0:
+            return 'Buzz'
+        else:
+            return number
 
-# for number in range(1,101):
-#      play_fizz_buzz=fizz_buzz()
-#      print(play_fizz_buzz)
+for number in range(1,101):
+     play_fizz_buzz=fizz_buzz()
+     print(play_fizz_buzz)
 
-# # EX 3 Password-strength checker
+# EX 3 Password-strength checker
 
-# def check_password(password):
+def check_password(password):
 
-#     problems = []
+    problems = []
 
-#     if len(password) <8 :
-#         problems.append('Password is too short')
+    if len(password) <8 :
+        problems.append('Password is too short')
 
-#     if not any(char.isdigit() for char in password):
-#         problems.append('Password needs a digit')
+    if not any(char.isdigit() for char in password):
+        problems.append('Password needs a digit')
 
     
-#     if not any(char.isupper() for char in password):
-#         problems.append('Password needs an uppercase letter')
+    if not any(char.isupper() for char in password):
+        problems.append('Password needs an uppercase letter')
 
-#     if not any(not char.isalpha() and not char.isdigit() for char in password):
-#         problems.append('Password needs a symbol')
+    if not any(not char.isalpha() and not char.isdigit() for char in password):
+        problems.append('Password needs a symbol')
 
-#     return problems
+    return problems
 
-# password=input('Enter the Password = ')
-# print (check_password(password))
+password=input('Enter the Password = ')
+print (check_password(password))
 
-# def number_guessing_game():
-#     attempts=0
-#     computer_number=random.randint(1,100)
-#     while True:
-#         number=int(input('Enter the Number = '))
-#         attempts+=1
-#         if number>computer_number:
-#             print('High')
-#         elif number<computer_number:
-#             print('low')
-#         else:
-#             print('Correct !')
-#             print(f'Attempts: {attempts}')
-#             break
+def number_guessing_game():
+    attempts=0
+    computer_number=random.randint(1,100)
+    while True:
+        number=int(input('Enter the Number = '))
+        attempts+=1
+        if number>computer_number:
+            print('High')
+        elif number<computer_number:
+            print('low')
+        else:
+            print('Correct !')
+            print(f'Attempts: {attempts}')
+            break
 
-# number_guessing_game()
+number_guessing_game()
 
 def to_title_case():
     sentence=input('Enter the Sentence = ')
